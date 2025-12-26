69b6675ef200435bf96756e6415c6bd030477e2f
Nathan <dr4x.14913@gmail.com>
2025-12-26 19:11:37 +0100

    feat: add basic auth

--- app.py
+++ app.py
@@ -1,8 +1,64 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, flash, redirect, url_for
 import json
+import os
 from pathlib import Path
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
+from werkzeug.security import check_password_hash
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'fallback-super-secret'
+
+HASHED_PASSWORD = 'scrypt:32768:8:1$xxnzWKJVYVZb9gAx$7ac30bebe582b4c63879304eb80dcbfb91f00a25996b454558045ce4b29254ce9265379a00eff1ba4b8d63134f3029c520c05c64882bc5dece0507810050e61d' # hash for 'toto'
+
+# Setup Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'admin_login'
+
+class AdminUser(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id  # Flask-Login requires 'id' attribute
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+@app.route('/admin_dashboard')
+@login_required
+def admin():
+    return "Hello"
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id == "admin":  # only one valid user
+        return AdminUser(user_id)
+    return None
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_login():
+    if current_user.is_authenticated:
+        return redirect(url_for('admin_dashboard'))
+    
+    form = LoginForm()
+    if form.validate_on_submit():
+        if (form.username.data == "admin" and 
+            check_password_hash(HASHED_PASSWORD, form.password.data)):
+            user = AdminUser("admin")
+            login_user(user)
+            return redirect('/admin_dashboard')
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def admin_logout():
+    logout_user()
+    return redirect(url_for('admin_login'))
 
 @app.route('/')
 def serve_index():
