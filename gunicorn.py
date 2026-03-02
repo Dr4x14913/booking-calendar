1dacb40fe651693b233ffcb7018ddd69d6d745f2
Nathan Daviaud <nathan.daviaud@allegrodvt.com>
2026-03-02 13:11:29 +0100

    gunicorn: reduce runners to 2

--- gunicorn.py
+++ gunicorn.py
@@ -1,5 +1,5 @@
 bind = "0.0.0.0:5000"  # Binding to 0.0.0.0 on port 5000
-workers = 4  # Number of worker processes
+workers = 2  # Number of worker processes
 timeout = 300
 
 loglevel = "info"  # Log level
